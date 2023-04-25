f3a3fbe252987b801ba60d34b564bf886759d422
rohit.sandawa <rohit.sandawa@JAISU1606183L.ad.infosys.com>
2023-04-25 13:25:06 +0530

    Initial Commit

--- GTXTest/src/test/java/UI/CheckGit.java
+++ GTXTest/src/test/java/UI/CheckGit.java
@@ -3,10 +3,10 @@ package UI;
 public class CheckGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-         //Changes made for first time
-		//Changes for secomd time
-		//changes for third time
+		/* TODO Auto-generated method stub
+         Changes made for first time
+		Changes for secomd time
+		changes for third time*/
 	}
 
 }
